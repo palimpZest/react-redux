7d924cee2b6b5cf4b0c8c525de005655c8fcd554
palimpZest <polo.vaca365@gmail.com>
2018-02-13 20:35:05 +0100

    feat: Add mongoose model and controller

---
+++ back/models/Task.js
@@ -0,0 +1,14 @@
+const config = require("../config.js");
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongodb, {
+  useMongoClient: true
+});
+const Schema = mongoose.Schema;
+
+const Task = new Schema({
+  name: String,
+  description: String
+});
+
+module.exports = mongoose.model("Task", Task);
