d5d1d869346bd11450ae399aad0c92f6ac6f667b
palimpZest <polo.vaca365@gmail.com>
2018-02-17 14:13:14 +0100

    feat: Create store

--- front-client/src/index.js
+++ front-client/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import { tasks } from "./reducers/tasks";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(tasks, applyMiddleware(thunk));
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+
 registerServiceWorker();
