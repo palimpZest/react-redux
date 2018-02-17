1ba857ab4c5ebebbecaf7d394574c33ef8b13dd1
palimpZest <polo.vaca365@gmail.com>
2018-02-17 13:34:01 +0100

    feat: Add form in homepage

--- front-client/src/App.js
+++ front-client/src/App.js
@@ -4,17 +4,28 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React-redux</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <h2>Write your tasks</h2>
+        <form action="/">
+          <label>Name</label>
+          <br />
+          <input type="text" name="name" placeholder="Your task name" />
+          <br />
+          <br />
+          <label>Description</label>
+          <br />
+          <input type="text" name="description" placeholder="Write description here" />
+          <br/>
+          <br/>
+          <button type="submit" onClick={{}}>
+            Add
+          </button>
+        </form>
+      </div>;
   }
 }
 
