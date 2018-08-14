c4b2ae8e7f8db19935dd28e70fbfd28506395c3d
palimpZest <polo.vaca365@gmail.com>
2018-08-14 19:27:30 +0200

    feat: Add edit task feature

--- front-client/src/actions/tasks.js
+++ front-client/src/actions/tasks.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const ADD_TASK_SUCCESS = 'ADD_TASK_SUCCESS';
+export const EDIT_TASK_SUCCESS = 'EDIT_TASK_SUCCESS';
 export const DELETE_TASK_SUCCESS = 'DELETE_TASK_SUCCESS';
 export const GET_ALL_TASKS_SUCCESS = 'GET_ALL_TASKS_SUCCESS';
 
@@ -12,18 +13,17 @@ function add_task_success(task) {
   };
 }
 
-function delete_task_success(task) {
+function edit_task_success(task) {
   return {
-    type: DELETE_TASK_SUCCESS,
+    type: EDIT_TASK_SUCCESS,
     task
   };
 }
 
-export function deleteTask(item) {
-  return dispatch => {
-    axios
-      .delete(`${API_URL}/${item}`)
-      .then(response => dispatch(delete_task_success(response.data)));
+function delete_task_success(task) {
+  return {
+    type: DELETE_TASK_SUCCESS,
+    task
   };
 }
 
@@ -36,6 +36,26 @@ export function addTask(item) {
     axios
       .post(API_URL, { name: item.name, description: item.description })
       .then(response => dispatch(add_task_success(response.data)));
+  };
+}
+
+export function editTask(item) {
+  return dispatch => {
+    axios
+      .put(`${API_URL}/${item.id}`, {
+        name: item.name,
+        description: item.description,
+        editing: item.editing
+      })
+      .then(response => dispatch(edit_task_success(response.data)));
+  };
+}
+
+export function deleteTask(item) {
+  return dispatch => {
+    axios
+      .delete(`${API_URL}/${item}`)
+      .then(response => dispatch(delete_task_success(response.data)));
   };
 }
 
--- front-client/src/reducers/tasks.js
+++ front-client/src/reducers/tasks.js
@@ -1,13 +1,34 @@
 import {
   ADD_TASK_SUCCESS,
+  EDIT_TASK_SUCCESS,
   DELETE_TASK_SUCCESS,
   GET_ALL_TASKS_SUCCESS
 } from '../actions/tasks';
 
-export function tasks(state = { tasks: [] }, action) {
+export function tasks(
+  state = {
+    tasks: []
+  },
+  action
+) {
   switch (action.type) {
     case ADD_TASK_SUCCESS:
       return { ...state, tasks: state.tasks.concat(action.task) };
+    case EDIT_TASK_SUCCESS:
+      return {
+        ...state,
+        tasks: state.tasks.map(task => {
+          if (task.id === action.id) {
+            return {
+              ...task,
+              name: task.name,
+              description: task.description
+            };
+          } else {
+            return task;
+          }
+        })
+      };
     case DELETE_TASK_SUCCESS:
       return { ...state, tasks: state.tasks.filter(id => id !== action.task) };
     case GET_ALL_TASKS_SUCCESS:
